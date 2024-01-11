e9e6d775934b086b0ddd875b0e50b98cdf84d09d
Sashank <sashankpaudel07@gmail.com>
2024-01-11 23:56:58 +0545

    final push

--- js/main.js
+++ js/main.js
@@ -1,91 +1,82 @@
-$(document).ready(function()
-{
+$(document).ready(function () {
     // navbar shrink
-    $(window).on("scroll",function()
-    {
-        if($(this).scrollTop() > 90)
-        {
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
             $(".navbar").addClass("navbar-shrink");
         }
-        else
-        {
+        else {
             $(".navbar").removeClass("navbar-shrink");
         }
     })
     // parallax js
-    function parallaxMouse()
-    {
-        if($('#parallax').length)
-        {
+    function parallaxMouse() {
+        if ($('#parallax').length) {
             var scene = document.getElementById('parallax');
             var parallax = new Parallax(scene);
         }
     }
     parallaxMouse();
     // skills bar
-    $(window).scroll(function()
-    {
+    $(window).scroll(function () {
         var hT = $("#skill-bar-wrapper").offset().top,
-        hH = $("#skill-bar-wrapper").outerHeight(),
-        wH = $(window).height(),
-        wS = $(this).scrollTop();
-        if( wS > (hT+hH-1.4*wH))
-        {
-            jQuery('.skillbar-container').each(function()
-            {
+            hH = $("#skill-bar-wrapper").outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - 1.4 * wH)) {
+            jQuery('.skillbar-container').each(function () {
                 jQuery(this).find('.skills').animate({
-                    width:jQuery(this).attr('data-percent')
+                    width: jQuery(this).attr('data-percent')
                 }, 5000) // 5 seconds
             })
         }
     })
     // filter
     let $btns = $('.img-gallery .sortBtn .filter-btn');
-    $btns.click(function(e) {
+    $btns.click(function (e) {
         $('.img-gallery .sortBtn .filter-btn').removeClass('active');
         e.target.classList.add('active');
         let selector = $(e.target).attr('data-filter');
         $('.img-gallery .grid').isotope
-        ({
-            filter:selector
-        })
+            ({
+                filter: selector
+            })
         return false;
     })
     $('.image-popup').magnificPopup
-    ({
-        type: 'image',
-        gallery: { enabled: true}
-    })
+        ({
+            type: 'image',
+            gallery: { enabled: true }
+        })
     // owl carousel
     $('.testimonial-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        responsiveClass:true,
-        autoplay:true,
-        responsive:{
-            0:{
-                items:1,
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:2,
+            600: {
+                items: 2,
             },
-            1000:{
-                items:3,
+            1000: {
+                items: 3,
             }
         }
     })
     // navbar collapse 
-    $(".nav-link").on("click",function()
-    {
+    $(".nav-link").on("click", function () {
         $(".navbar-collapse").collapse("hide");
     })
     // scroll
     $.scrollIt({
-        topOffset:-50
+        topOffset: -50
     })
-    
+
     var audio = new Audio('../happybirthday.mp3');
     audio.play();
     // audio.pause();
-    
+
 })
+
